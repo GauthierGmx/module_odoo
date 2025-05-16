c0403ebf57510f125e8a27948fc1530aab410fa8
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2025-05-16 08:44:10 +0200

    Retrait de la contrainte + ajout du code ISO dans le formulaire

--- postal/models/postal_region.py
+++ postal/models/postal_region.py
@@ -8,10 +8,4 @@ class Region(models.Model):
 
     name = fields.Char(string='Name', required=True)
     iso_3166_code = fields.Char(string='ISO 3166 Code', required=True)
-    insee_code = fields.Char(string='INSEE Code', required=True, size=2)
-
-    @api.constrains('insee_code')
-    def _check_insee_code(self):
-        for record in self:
-            if not record.insee_code.isdigit() or len(record.insee_code) != 2:
-                raise ValidationError("The INSEE code must be a 2-digit number.")
+    insee_code = fields.Char(string='INSEE Code', required=True)
