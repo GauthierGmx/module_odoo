f678f469dc443ab063fa7e73634b90071a408523
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2025-05-16 09:44:13 +0200

    Ajout du compte de département par région

--- postal/models/postal_region.py
+++ postal/models/postal_region.py
@@ -10,3 +10,9 @@ class Region(models.Model):
     iso_3166_code = fields.Char(string='ISO 3166 Code', required=True)
     insee_code = fields.Char(string='INSEE Code', required=True)
     departement_ids = fields.One2many('postal.departement','region_id',string='Departments')
+    departement_count = fields.Integer(string='Number of Departments', compute='_compute_departement_count', store=True)
+
+    @api.depends('departement_ids')
+    def _compute_departement_count(self):
+        for record in self:
+            record.departement_count = len(record.departement_ids)
