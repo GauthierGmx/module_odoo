a005bcf81c9955625997356f52f9178e83316834
Gauthier GOUMEAUX <gauthier.goumeaux640@gmail.com>
2025-05-16 10:19:38 +0200

    Modification de la vue formulaire

--- postal/__manifest__.py
+++ postal/__manifest__.py
@@ -15,7 +15,7 @@ Addon used to manage postal informations in France such as: region, department, 
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localization',
-    'version': '0.5',
+    'version': '0.6.1',
     'license': 'AGPL-3',
     'application': True,
     'images': '/static/description/icon.png',
